94caf17baa70f93223b678a338f2d04ded815973
Ardi Germenji <67866525+a-pyro@users.noreply.github.com>
2020-10-04 12:10:54 +0200

    findodd added

--- playground.js
+++ playground.js
@@ -83,10 +83,43 @@ function order(words) {
 
 }
 
-console.log(order("is2 Thi1s T4est 3a"));
+// console.log(order("is2 Thi1s T4est 3a"));
 
 // console.log(isNaN('g'));
 
 // for (let char of 'hello') {
 //     console.log(char);
 // }
+
+
+/* Given an array of integers, find the one that appears an odd number of times.
+
+There will always be only one integer that appears an odd number of times. */
+
+function findOdd(numbers) {
+    // increase the counter at each match
+    // if the counter is odd i should have found the number
+    // i need modulo check on counter
+    // counter should be emptied at each iteration // need to check if % !==0 and return to break the loop
+
+    // take the first item and check its equality against each other element
+    for (let fixed of numbers) {
+        let counter = 0;
+        for (let element of numbers) {
+
+            if (fixed === element) {
+                counter = counter + 1;
+
+            }
+
+        }
+
+        if (counter % 2 !== 0) {
+            return fixed;
+        }
+
+    }
+
+}
+
+console.log(findOdd([20, 1, 1, 2, 2, 3, 3, 5, 5, 4, 20, 4, 5]));
