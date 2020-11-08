2f936c22b1ecb61e9aaf5fe93a1b482f89b991f8
Ardi Germenji <67866525+a-pyro@users.noreply.github.com>
2020-11-08 14:32:53 +0100

    Update playground.js

--- playground.js
+++ playground.js
@@ -585,59 +585,74 @@ const ranger = (n1, n2) => {
 Create a function to create a new string of specified copies (positive number) of a given string.
 */
 const fotocopiatrice = (text, n) => (n > 0 ? text.repeat(n) : 'invalid number');
-console.log(fotocopiatrice('ciao', 2));
-console.log(fotocopiatrice('ciao', 1));
-console.log(fotocopiatrice('ciao', 0));
+// console.log(fotocopiatrice('ciao', 2));
+// console.log(fotocopiatrice('ciao', 1));
+// console.log(fotocopiatrice('ciao', 0));
 /*
 7)
 Create a function to display the city name if the string begins with "Los" or "New" otherwise return blank.
 */
+const city = (text) =>
+    text.toLowerCase().slice(0, 3).includes('los') ||
+    text.toLowerCase().slice(0, 3).includes('new')
+        ? text
+        : 'blank!';
+// console.log(city('ciaone'));
+// console.log(city('loscianoe'));
+// console.log(city('newone'));
+// console.log(city('onenew'));
+// console.log(city('nelosne'));
 
 /*
 8)
 Create a function to calculate the sum of three elements of a given array of integers of length 3.
 */
-
+const sumArr = (arr) => arr.reduce((ac, cv) => ac + cv);
+// console.log(sumArr([3, 6, 9]));
 /*
 9)
 Create a function to test whether an array of integers of length 2 contains 1 or a 3. 
 */
-
+const checkArr = (arr) => arr.includes(1) || arr.includes(3);
+// console.log(checkArr([0, 5]));
+// console.log(checkArr([1, 5]));
+// console.log(checkArr([3, 5]));
 /*
 10)
 Create a function to test whether an array of integers of length 2 does not contain 1 or a 3
-11)
-Create a function to find the longest string from a given array of strings.
-12)
-Create a function to find the types of a given angle.
-Types of angles:
-    Acute angle: An angle between 0 and 90 degrees.
-    Right angle: An 90 degree angle.
-    btuse angle: An angle between 90 and 180 degrees.
-    Straight angle: A 180 degree angle.
-13)
-Create a function to find the index of the greatest element of a given array of integers
-14)
-Create a function to get the largest even number from an array of integers.
-16)
-Create a function to check from two given integers, whether one is positive and another one is negative.
-17)
-Create a function to create new string with first 3 characters are in lower case and the others in upper case. If the string length is less than 3 convert all the characters in upper case. 
-18)
-Create a function to calculate the sum of the two given integers, If the sum is in the range 50..80 return 65 other wise return 80.
-19)
-Create a function to convert a number to a string, the contents of which depend on the number's factors. Follow next example:
-If the number has 3 as a factor, output 'Diego'.
-If the number has 5 as a factor, output 'Riccardo'.
-If the number has 7 as a factor, output 'Stefano'.
-If the number does not have 3, 5, or 7 as a factor, just pass the number's digits straight through.
-Examples
-28's factors are 1, 2, 4, 7, 14, 28.
-this would be a simple "Stefano".
-30's factors are 1, 2, 3, 5, 6, 10, 15, 30.
-this would be a "DiegoRiccardo".
-34 has four factors: 1, 2, 17, and 34.
-this would be "34".
-20)
-Create a function that given a phrase returns its acronym, like British Broadcasting Corporation returns BBC
 */
+const checkArr2 = (arr) => !arr.includes(1) || arr.includes(3);
+// 11)
+// Create a function to find the longest string from a given array of strings.
+// 12)
+// Create a function to find the types of a given angle.
+// Types of angles:
+//     Acute angle: An angle between 0 and 90 degrees.
+//     Right angle: An 90 degree angle.
+//     btuse angle: An angle between 90 and 180 degrees.
+//     Straight angle: A 180 degree angle.
+// 13)
+// Create a function to find the index of the greatest element of a given array of integers
+// 14)
+// Create a function to get the largest even number from an array of integers.
+// 16)
+// Create a function to check from two given integers, whether one is positive and another one is negative.
+// 17)
+// Create a function to create new string with first 3 characters are in lower case and the others in upper case. If the string length is less than 3 convert all the characters in upper case.
+// 18)
+// Create a function to calculate the sum of the two given integers, If the sum is in the range 50..80 return 65 other wise return 80.
+// 19)
+// Create a function to convert a number to a string, the contents of which depend on the number's factors. Follow next example:
+// If the number has 3 as a factor, output 'Diego'.
+// If the number has 5 as a factor, output 'Riccardo'.
+// If the number has 7 as a factor, output 'Stefano'.
+// If the number does not have 3, 5, or 7 as a factor, just pass the number's digits straight through.
+// Examples
+// 28's factors are 1, 2, 4, 7, 14, 28.
+// this would be a simple "Stefano".
+// 30's factors are 1, 2, 3, 5, 6, 10, 15, 30.
+// this would be a "DiegoRiccardo".
+// 34 has four factors: 1, 2, 17, and 34.
+// this would be "34".
+// 20)
+// Create a function that given a phrase returns its acronym, like British Broadcasting Corporation returns BBC
