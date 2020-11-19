9650acb5760a2849174aed7320a6af1042681f49
Ardi Germenji <67866525+a-pyro@users.noreply.github.com>
2020-11-19 14:26:26 +0100

    plusMinus ex

--- playground.js
+++ playground.js
@@ -745,8 +745,20 @@ const acronymr = (string) =>
 // console.log(acronymr('British Broadcasting Corporation'));
 // console.log(acronymr('British Cazzoni Corporation'));
 // console.log(acronymr('America Broadcasting Corporation Defe eee'));
-function sumPairs(ints, s) {
-    return [0, 0] || undefined;
+
+/*
+Given an array of integers, calculate the ratios of its elements that are positive, negative, and zero. Print the decimal value of each fraction on a new line with  places after the decimal.
+
+Note: This challenge introduces precision problems. The test cases are scaled to six decimal places, though answers with absolute error of up to  are acceptable.*/
+
+function plusMinus(arr) {
+    const arrLen = arr.length;
+    const positives = arr.filter((el) => el > 0).length;
+    const negatives = arr.filter((el) => el < 0).length;
+    const zeros = arr.filter((el) => el === 0).length;
+    console.log((positives / arrLen).toFixed(6));
+    console.log((negatives / arrLen).toFixed(6));
+    console.log((zeros / arrLen).toFixed(6));
 }
 
-console.log(sumPairs());
+plusMinus([-4, 3, -9, 0, 4, 1]);
