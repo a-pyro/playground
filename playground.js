4cd2717a0c5e6d0f13b4a8d53aa899748c3da361
ardigermenji <ardi.germenji@gmail.com>
2021-01-14 21:21:26 +0100

    anagrams

--- playground.js
+++ playground.js
@@ -1013,3 +1013,44 @@ function scramble(str1, str2) {
 
 // scramble('rkqodlw', 'world');
 // scramble('katazs', 'steak');
+
+/* 
+What is an anagram? Well, two words are anagrams of each other if they both contain the same letters. For example:
+
+'abba' & 'baab' == true
+
+'abba' & 'bbaa' == true
+
+'abba' & 'abbba' == false
+
+'abba' & 'abca' == false
+Write a function that will find all the anagrams of a word from a list. You will be given two inputs a word and an array with words. You should return an array of all the anagrams or an empty array if there are none. 
+
+anagrams('abba', ['aabb', 'abcd', 'bbaa', 'dada']) => ['aabb', 'bbaa']
+
+anagrams('racer', ['crazer', 'carer', 'racar', 'caers', 'racer']) => ['carer', 'racer']
+
+anagrams('laser', ['lazing', 'lazy',  'lacer']) => []
+*/
+
+function anagrams(word, words) {
+    const letters = [...word];
+    const anagrams = words.filter(word => {
+        const toCheck = [...word];
+        letters.forEach(letter => {
+            if (toCheck.includes(letter)) {
+                toCheck.splice(toCheck.indexOf(letter), 1);
+            }
+        });
+        if (toCheck.length === 0) return true;
+    });
+
+    console.log(anagrams);
+    return anagrams;
+}
+
+anagrams('abba', ['aabb', 'abcd', 'bbaa', 'dada']);
+
+anagrams('racer', ['crazer', 'carer', 'racar', 'caers', 'racer']);
+
+anagrams('laser', ['lazing', 'lazy', 'lacer']);
