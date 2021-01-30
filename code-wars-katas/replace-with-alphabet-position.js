b997c2c714ef779b210a55c88c5563bd8865a3dc
ardigermenji <ardi.germenji@gmail.com>
2021-01-30 20:55:34 +0100

    replace-with-alphabet-position

--- code-wars-katas/replace-with-alphabet-position.js
+++ code-wars-katas/replace-with-alphabet-position.js
@@ -9,3 +9,24 @@ If anything in the text isn't a letter, ignore it and don't return it.
 Example
 alphabetPosition("The sunset sets at twelve o' clock.")
 Should return "20 8 5 19 21 14 19 5 20 19 5 20 19 1 20 20 23 5 12 22 5 15 3 12 15 3 11" (as a string) */
+
+function alphabetPosition(text) {
+  const alpha = [...'abcdefghijklmnopqrstuvwxyz'];
+  console.log(text);
+  return text
+    .toLowerCase()
+    .split('')
+    .map((letter) => alpha.indexOf(letter))
+    .filter((el) => el >= 0)
+    .map((el) => el + 1)
+    .join(' ');
+}
+
+console.log(
+  alphabetPosition("The sunset sets at twelve o' clock."),
+  '20 8 5 19 21 14 19 5 20 19 5 20 19 1 20 20 23 5 12 22 5 15 3 12 15 3 11'
+);
+console.log(
+  alphabetPosition('The narwhal bacons at midnight.'),
+  '20 8 5 14 1 18 23 8 1 12 2 1 3 15 14 19 1 20 13 9 4 14 9 7 8 20'
+);
