0d4f9c039896e919e02354a39314f93f8b71bbce
ardigermenji <ardi.germenji@gmail.com>
2021-01-30 21:20:07 +0100

    funky composition

---
+++ code-wars-katas/unary-function-chainer.js
@@ -0,0 +1,15 @@
+/* Your task is to write a higher order function for chaining together a list of unary functions. In other words, it should return a function that does a left fold on the given functions.
+
+chained([a,b,c,d])(input)
+Should yield the same result as
+
+d(c(b(a(input)))) */
+// function composition
+function chained(functions) {
+  return function (args) {
+    return functions.reduce((arg, fn) => fn(arg), args);
+  };
+}
+
+const chained = (functions) => (args) =>
+  functions.reduce((arg, fn) => fn(arg), args);
